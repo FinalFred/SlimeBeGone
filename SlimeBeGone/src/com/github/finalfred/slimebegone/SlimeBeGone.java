b2052a895752452dd972f892caa1fff2225b210a
FinalFred <Fabian@Fabian-PC>
2012-11-30 17:39:04 -0500

    faew

---
+++ SlimeBeGone/src/com/github/finalfred/slimebegone/SlimeBeGone.java
@@ -0,0 +1,42 @@
+package com.github.finalfred.slimebegone;
+
+import org.bukkit.Location;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SlimeBeGone extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onCreatureSpawn(CreatureSpawnEvent e) {
+		if (e.getEntityType() != EntityType.SLIME) {
+			// Don't care
+			return;
+		}
+		
+		Location l = e.getLocation();
+		
+		// Verify > 60 requirement
+		if (l.getY() > 60) {
+			
+			// Verify Spawp biome
+			if (l.getWorld().getBiome(l.getBlockX(), l.getBlockZ()) == Biome.SWAMPLAND) {
+				
+				// Slime is fine
+				return;
+			}
+		}
+		
+		// Nope! No spawning!
+		e.setCancelled(true);
+	}
+}
