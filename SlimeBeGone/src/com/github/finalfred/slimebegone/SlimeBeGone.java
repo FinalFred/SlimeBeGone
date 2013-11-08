bef5ce48b109434b625de9205b3469591a96b77b
Fabian <fthielisch@gmail.com>
2013-11-08 15:03:58 -0500

    Fixed imports

--- SlimeBeGone/src/com/github/finalfred/slimebegone/SlimeBeGone.java
+++ SlimeBeGone/src/com/github/finalfred/slimebegone/SlimeBeGone.java
@@ -7,7 +7,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SlimeBeGone extends JavaPlugin implements Listener {
